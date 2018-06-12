659a035a11a59750df765ad949fe2219f10108c3
dalma <dalma.foldesi@gmail.com>
2018-06-12 12:14:34 +0200

    Initial poly files

---
+++ RectanglePack/PolygonFindMajorDirection.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace RectanglePack
+{
+    public class PolygonFindMajorDirection : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public PolygonFindMajorDirection()
+          : base("PolygonFindMajorDirection", "PolygonMajorDir",
+              "Find the major axis / direction of closed polyline curve",
+              "Polyline", "Utility")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("polygon", "p", "Polygon to find major axis of", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("numAngleCategories", "nCat", "Number of possible categories (bins) for angles", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("debug", "debug", "Toggle debug mode", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddNumberParameter("orientation", "or", "Orientation or major axis of input polygon", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            double or = 0.0;
+            PolyCurve polygon = new PolyCurve();
+            int numCategories = 0;
+            bool debug = false;
+
+            DA.GetData(0, ref polygon);
+            DA.GetData(1, ref numCategories);
+            DA.GetData(2, ref debug);
+
+            if (polygon.IsValid && !polygon.IsShort(Rhino.RhinoMath.ZeroTolerance) && polygon.IsClosed)
+            {
+                FindMajorEdgeOrientation(polygon, ref or, numCategories, debug);
+            }
+            else
+                this.AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Input polygon / curve is invalid, too short, or not closed");
+
+            DA.SetData(0, or);
+        }
+
+        private void FindMajorEdgeOrientation(Curve polygon, ref double orientation, int numCategories, bool debug)
+        {
+            List<Curve> segments = polygon.DuplicateSegments().ToList();
+            CurveOrientation co = CurveOrientation.Clockwise;
+            List<double> lengthTable = CreateTable(numCategories);
+            Dictionary<int, int> curveTable = new Dictionary<int, int>();
+            Vector3d xyVector = Vector3d.XAxis;
+            double angleBinSize = ((2.0 * Math.PI) / numCategories);
+
+            if (numCategories < segments.Count)
+                numCategories = segments.Count;
+
+            for (int i = 0; i < segments.Count; i++)
+            {
+                if (segments[i].ClosedCurveOrientation() == co)
+                    segments[i].Reverse();
+
+                double currLength = segments[i].GetLength();
+
+                Vector3d segmentVector = Vector3d.Subtract(new Vector3d(segments[i].PointAtEnd), new Vector3d(segments[i].PointAtStart));
+                Vector3d cross = Vector3d.CrossProduct(xyVector, segmentVector);
+                double currAngle = Vector3d.VectorAngle(segmentVector, xyVector, cross);
+
+                int category = (int)(currAngle / angleBinSize);
+                if (!curveTable.ContainsKey(category))
+                    curveTable[category] = i;
+
+                if (debug)
+                {
+                    Rhino.RhinoApp.WriteLine("INDEX " + i.ToString());
+                    Rhino.RhinoApp.WriteLine("ANGLE " + Rhino.RhinoMath.ToDegrees(currAngle).ToString());
+                    Rhino.RhinoApp.WriteLine("CATEGORY " + category.ToString());
+                }
+                lengthTable[category] += currLength;
+            }
+
+            // Find maximum element in length array
+            double max = lengthTable.Max();
+            int argmax = Array.IndexOf(lengthTable.ToArray(), max);
+
+            if (debug) {
+                Rhino.RhinoApp.WriteLine("max " + max.ToString());
+                Rhino.RhinoApp.WriteLine("argmax " + argmax.ToString());
+            }
+            
+            orientation = argmax * ((2.0 * Math.PI) / numCategories);
+
+            // Double-check curve orientation
+            Curve maxCurve = segments[curveTable[argmax]];
+            Vector3d maxCurveVector = Vector3d.Subtract(new Vector3d(maxCurve.PointAtEnd), new Vector3d(maxCurve.PointAtStart));
+            Line checkLine = new Line(maxCurve.PointAtStart, xyVector);
+            checkLine.Transform(Transform.Rotation(orientation, maxCurve.PointAtStart));
+
+            double angle = Vector3d.VectorAngle(maxCurveVector, checkLine.Direction);
+            if (angle > angleBinSize)
+                orientation = Math.PI - orientation;
+
+        }
+
+        private double AngleBetween(Vector3d vector1, Vector3d vector2)
+        {
+            double sin = vector1.X * vector2.Y - vector2.Y * vector1.Y;
+            double cos = vector1.X * vector2.X + vector1.Y * vector2.Y;
+
+            return Math.Atan2(sin, cos) * (180 / Math.PI);
+
+        }
+
+
+        private List<double> CreateTable(int numCategories)
+        {
+            List<double> lengthTable = new List<double>();
+
+            for (int i = 0; i < numCategories; i++)
+            {
+                lengthTable.Add(0);
+            }
+            return lengthTable;
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("9ef00dac-fa89-4eb8-96ec-af9e9e24fc9a"); }
+        }
+    }
+}
---
+++ RectanglePack/RectanglePackComponent.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace RectanglePack
+{
+    public class RectanglePackComponent : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public RectanglePackComponent()
+          : base("RectanglePack", "Nickname",
+              "Pack boundary curve with rectangles",
+              "Category", "Subcategory")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Boundaries", "Bnd", "List of one or more bounaries to pack", GH_ParamAccess.list);
+            pManager.AddCurveParameter("Rectangle", "Rect", "Rectangle", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Orientation", "Or", "Orientation of rectangles in each boundary", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Offset", "O", "Offset", GH_ParamAccess.item);
+            pManager.AddNumberParameter("SearchSpacing", "Search", "Spacing while searching for initial fit", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("bRotation", "bR", "Should rotate rectangles or not", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("bAxisAligned", "bA", "Axis aligned rectangle orientation", GH_ParamAccess.item);
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddCurveParameter("PackedRectangles", "Packed", "Rectangles that have been fit", GH_ParamAccess.item);
+            pManager.AddPointParameter("GridPoints", "GP", "Base points for grid alignment", GH_ParamAccess.list);
+            pManager.AddVectorParameter("RectangleVectors", "RV", "Rectangle move vectors", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            List<Curve> boundaries = new List<Curve>();
+            Rectangle3d fitRect = new Rectangle3d();
+            List<double> orientations = new List<double>();
+            bool bRotation = false;
+            bool bAxisAligned = false;
+            double searchSpacing = 1.0;
+            double offset = 5.0;
+
+            DA.GetData(0, ref boundaries);
+            DA.GetData(1, ref fitRect);
+            DA.GetData(2, ref orientations);
+            DA.GetData(3, ref offset);
+            DA.GetData(4, ref searchSpacing);
+            DA.GetData(5, ref bRotation);
+            DA.GetData(6, ref bAxisAligned);
+
+            List<Rectangle3d> foundRects = new List<Rectangle3d>();
+            List<Point3d> pts = new List<Point3d>();
+            List<Vector3d> vs = new List<Vector3d>();
+
+            foreach (Curve boundary in boundaries)
+            {
+                if (fitRect.IsValid)
+                {
+                    BoundingBox bx = boundary.GetBoundingBox(false);
+                    foundRects.AddRange(CheckFit(bx, fitRect, boundary, offset, bRotation, pts, searchSpacing, bAxisAligned, vs));
+                }
+            }
+
+            DA.SetData(0, foundRects);
+            DA.SetData(1, pts);
+            DA.SetData(2, vs);
+        }
+
+        private void RunScript(Curve boundary, double offset, double searchSpacing, List<Rectangle3d> fitRects, bool withRotation, object withPartition, bool axisAligned, ref object packedRects, ref object outPts, ref object moveVs)
+        {
+
+            BoundingBox bx = boundary.GetBoundingBox(false);
+            List<Rectangle3d> foundRects = new List<Rectangle3d>();
+            packedRects = new List<Rectangle3d>();
+            List<Point3d> pts = new List<Point3d>();
+            List<Vector3d> vs = new List<Vector3d>();
+
+            foreach (Rectangle3d fitRect in fitRects)
+            {
+                if (fitRect.IsValid)
+                {
+                    foundRects.AddRange(CheckFit(bx, fitRect, boundary, offset, withRotation, pts, searchSpacing, axisAligned, vs));
+                }
+            }
+
+            packedRects = foundRects;
+            outPts = pts;
+            moveVs = vs;
+        }
+
+        // <Custom additional code> 
+        private List<double> FindSpacing(Rectangle3d rect, double offset)
+        {
+            List<double> spacing = new List<double>(2);
+            double dX = 0;
+            double dY = 0;
+
+            Vector3d v1 = rect.Plane.XAxis;
+            Vector3d v2 = Plane.WorldXY.XAxis;
+
+            Vector3d translationVector = new Vector3d(rect.Plane.XAxis.X, rect.Plane.YAxis.Y, 1);
+            // translationVector.Unitize();
+
+
+            if (Vector3d.VectorAngle(v1, v2) < 0.5)
+            {
+                dX = (offset * 2 + rect.Width) * translationVector.X;
+                dY = (offset * 2 + rect.Height) * translationVector.Y;
+            }
+            else
+            {
+                dX = (offset * 2 + rect.Height) * translationVector.Y;
+                dY = (offset * 2 + rect.Width) * translationVector.X;
+            }
+
+
+            /* dX = (offset + rect.Width) * translationVector.X;
+             dY = (offset + rect.Height) * translationVector.Y; */
+
+            spacing.Add(dX);
+            spacing.Add(dY);
+
+            return spacing;
+        }
+
+        private IList<Rectangle3d> CheckFit(BoundingBox bx, Rectangle3d rect, Curve boundary, double offset, bool withRotation, List<Point3d> outPts, double searchSpacing, bool axisAligned, List<Vector3d> vs)
+        {
+            bool IsRotated = false;
+            bool searching = true;
+
+            double angle = 1.5708;
+
+            List<double> spacing = FindSpacing(rect, offset);
+            IList<Rectangle3d> tRects = new List<Rectangle3d>();
+
+            double dX = searchSpacing;
+            double dY = searchSpacing;
+            double xMin = bx.Min.X;
+            double yMin = bx.Min.Y;
+            Point3d p = new Point3d(xMin, yMin, 0.0);
+
+            Vector3d translationVector = new Vector3d(rect.Plane.XAxis.X, rect.Plane.YAxis.Y, 1);
+            translationVector.Unitize();
+
+            for (double y = yMin; y < bx.Max.Y; y += dY)
+            {
+                for (double x = xMin; x < bx.Max.X; x += dX)
+                {
+                    Vector3d v = new Vector3d(x, y, 0.0);
+                    Vector3d pv = new Vector3d(p);
+
+                    if (axisAligned)
+                    {
+                        pv = new Vector3d(translationVector.X * dX, translationVector.Y * dY, 1);
+                    }
+                    Vector3d toVector = Vector3d.Add(v, pv);
+                    Point3d pNew = new Point3d(toVector);
+
+                    p = pNew;
+
+                    PointContainment pc = boundary.Contains(p, Plane.WorldXY, 0.1);
+
+                    if (pc == PointContainment.Inside)
+                    {
+
+                        // Translate rect to new position
+                        Rectangle3d tRect = TranslateRect(p, rect);
+
+                        // Check containment of rect
+                        bool IsContained = BoundaryContainsCurve(tRect.ToNurbsCurve(), boundary);
+
+                        if (!IsContained && withRotation && !searching)
+                        {
+                            tRect.Transform(Transform.Rotation(angle, p));
+                            IsContained = BoundaryContainsCurve(tRect.ToNurbsCurve(), boundary);
+                            IsRotated = true;
+                        }
+
+                        if (IsContained)
+                        {
+                            if (searching)
+                            {
+                                searching = false;
+                                dX = spacing[0];
+                                dY = spacing[1];
+                                xMin = (1 - bx.Min.X % p.X) * p.X;
+                            }
+                            outPts.Add(p);
+                            vs.Add(pv);
+                            tRects.Add(tRect);
+
+                        }
+                        if (IsRotated && withRotation)
+                        {
+                            // Update spacing
+                            double t = dX;
+                            dX = dY;
+                            dY = t;
+                            IsRotated = false;
+                        }
+                    }
+                }
+            }
+
+            return tRects;
+        }
+
+        private Rectangle3d TranslateRect(Point3d pt, Rectangle3d r0)
+        {
+            Vector3d c0 = new Vector3d(AreaMassProperties.Compute(r0.ToNurbsCurve()).Centroid);
+            Vector3d tg = new Vector3d(pt);
+            Vector3d t0 = Vector3d.Subtract(tg, c0);
+
+            Rectangle3d rNew0 = new Rectangle3d(r0.Plane, r0.Width, r0.Height);
+            rNew0.Transform(Transform.Translation(t0));
+
+            return rNew0;
+        }
+
+        private bool BoundaryContainsCurve(Curve shape, Curve boundary)
+        {
+            bool IsContained = false;
+            List<Point3d> vertices = shape.ToNurbsCurve().Points.Select(p => p.Location).ToList();
+
+            for (int i = 0; i < vertices.Count; i++)
+            {
+                Point3d vertex = vertices[i];
+                PointContainment pContainment = boundary.Contains(vertex, Plane.WorldXY, 0.00001);
+                if (pContainment.Equals(PointContainment.Inside)) IsContained = true;
+                else
+                {
+                    IsContained = false;
+                    break;
+                }
+            }
+
+            return IsContained;
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("0aff020c-20a1-4ae8-92d0-f7092210291c"); }
+        }
+    }
+}
